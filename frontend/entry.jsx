974051d9cf232d0980f0f35bd857c1752319060b
cameron mcinnes <cam.mcinnes@gmail.com>
2018-04-17 12:48:11 -0400

    Add footer and mobile first media query

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import ForecastContainer from './forecast_container';
 
 const Root = () => {
-  return <ForecastContainer />;
+  return (
+    <main>
+      <ForecastContainer />
+      <footer>
+        <h4>Cameron McInnes for C4Q</h4>
+      </footer>
+    </main>
+  );
 }
 
-// add dom content loaded callback to ensure page fully loaded 
+// add dom content loaded callback to ensure page fully loaded
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root/>, document.getElementById('root'));
 });
